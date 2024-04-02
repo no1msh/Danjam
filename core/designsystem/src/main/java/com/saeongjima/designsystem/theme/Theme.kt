a3e5a3d8cca597ff2c8e520bcef6c8e82646cc5e
Bandal <no1msh1217@gmail.com>
2024-04-03 01:07:09 +0900

    feat: 상태바 아이콘들의 색상이 앱과 어울리게 조정

--- core/designsystem/src/main/java/com/saeongjima/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/saeongjima/designsystem/theme/Theme.kt
@@ -1,17 +1,13 @@
 package com.saeongjima.designsystem.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -21,6 +17,7 @@ private val DarkColorScheme = darkColorScheme(
     tertiary = PointColor2,
     error = Error,
     background = Black100,
+    onBackground = Black950,
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -29,46 +26,31 @@ private val LightColorScheme = lightColorScheme(
     tertiary = PointColor2,
     error = Error,
     background = Black100,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = Black950,
 )
 
 @Composable
 fun DanjamTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    // dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-//            val window = (view.context as Activity).window
-//            window.statusBarColor = colorScheme.primary.toArgb()
-//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
             val window = (view.context as Activity).window
             val insetsController = WindowCompat.getInsetsController(window, view)
             window.statusBarColor = colorScheme.background.toArgb()
             window.navigationBarColor = colorScheme.background.toArgb()
-            insetsController.isAppearanceLightStatusBars = !darkTheme
-            insetsController.isAppearanceLightNavigationBars = !darkTheme
+
+            // 항상 다크테마이기 때문에 상태바 아이콘들을 밝은 톤으로 표시
+            insetsController.isAppearanceLightStatusBars = false
+            insetsController.isAppearanceLightNavigationBars = false
         }
     }
 
