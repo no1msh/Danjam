1da4e2ff4cf18ed93ec51c43157058abf4a85d34
Bandal <no1msh1217@gmail.com>
2024-04-14 16:05:29 +0900

    feat: Typography 디자인 요구사항에 맞춰 최신화

--- core/designsystem/src/main/java/com/saeongjima/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/saeongjima/designsystem/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.saeongjima.designsystem.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -21,59 +22,100 @@ val Typography = Typography(
         fontFamily = wantedSans,
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
     displayMedium = TextStyle(
         fontFamily = wantedSans,
         fontSize = 20.sp,
         fontWeight = FontWeight.SemiBold,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
 
     headlineLarge = TextStyle(
         fontFamily = wantedSans,
         fontSize = 18.sp,
         fontWeight = FontWeight.SemiBold,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
     headlineMedium = TextStyle(
         fontFamily = wantedSans,
         fontSize = 18.sp,
         fontWeight = FontWeight.Medium,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
 
     titleLarge = TextStyle(
         fontFamily = wantedSans,
         fontSize = 16.sp,
         fontWeight = FontWeight.SemiBold,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
     titleMedium = TextStyle(
         fontFamily = wantedSans,
         fontSize = 16.sp,
         fontWeight = FontWeight.Medium,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
     titleSmall = TextStyle(
         fontFamily = wantedSans,
         fontSize = 16.sp,
         fontWeight = FontWeight.Normal, // Regular
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
 
     bodyLarge = TextStyle(
         fontFamily = wantedSans,
         fontSize = 14.sp,
-        fontWeight = FontWeight.Normal, // Regular
+        fontWeight = FontWeight.SemiBold,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
     bodyMedium = TextStyle(
         fontFamily = wantedSans,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal, // Regular
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
+    ),
+    bodySmall = TextStyle(
+        fontFamily = wantedSans,
         fontSize = 12.sp,
         fontWeight = FontWeight.SemiBold,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
 
     labelLarge = TextStyle(
         fontFamily = wantedSans,
         fontSize = 12.sp,
         fontWeight = FontWeight.Normal, // Regular
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
     labelMedium = TextStyle(
         fontFamily = wantedSans,
         fontSize = 8.sp,
         fontWeight = FontWeight.Normal, // Regular
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false,
+        ),
     ),
 )
