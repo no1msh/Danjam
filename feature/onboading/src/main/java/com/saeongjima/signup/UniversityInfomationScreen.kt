f0f18f4e461e7f70c8aa1f9a4aa236ce7dbed363
Bandal <no1msh1217@gmail.com>
2024-04-17 23:07:58 +0900

    feat: 학교 정보 입력 화면 구현

--- feature/onboading/src/main/java/com/saeongjima/signup/UniversityInfomationScreen.kt
+++ feature/onboading/src/main/java/com/saeongjima/signup/UniversityInfomationScreen.kt
@@ -5,21 +5,30 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.saeongjima.designsystem.component.DanjamButton
-import com.saeongjima.designsystem.theme.Black500
-import com.saeongjima.designsystem.theme.DanjamTheme
+import com.saeongjima.designsystem.component.DanjamTextField
+import com.saeongjima.designsystem.theme.Black950
 import com.saeongjima.designsystem.theme.White
-import com.saeongjima.signin.InputField
+import com.saeongjima.signup.departmentselect.DepartmentSelectScreen
 
 @Composable
 fun UniversityInformationScreen(modifier: Modifier = Modifier) {
+    var openDialog by remember { mutableStateOf(false) }
+
+    if (openDialog) {
+        DepartmentSelectScreen {
+            openDialog = false
+        }
+    }
+
     Column(modifier = modifier) {
         Text(
             text = "학교 정보",
@@ -27,41 +36,44 @@ fun UniversityInformationScreen(modifier: Modifier = Modifier) {
             color = White,
         )
         Spacer(modifier = Modifier.height(44.dp))
-        InputField(title = "입학년도", hint = "EX) 2024", modifier = Modifier.fillMaxWidth())
+        InputBox(title = "입학년도", modifier = Modifier.fillMaxWidth()) {
+            DanjamTextField(
+                value = "",
+                onValueChange = {},
+                hintText = "EX) 2024"
+            )
+        }
         Spacer(modifier = Modifier.height(32.dp))
-        InputField(
+        InputBox(
             title = "학교",
-            hint = "EX) 단잠대학교",
             modifier = Modifier.fillMaxWidth(),
-        )
-        InputField(
+        ) {
+            DanjamTextField(value = "", onValueChange = {}, hintText = "EX) 단잠대학교")
+        }
+        InputBox(
             title = "학과",
-            hint = "EX) 단잠과",
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 32.dp),
-            trailing = {
-                DanjamButton(
-                    text = "검색하기",
-                    textStyle = MaterialTheme.typography.bodyMedium,
-                    textColor = White,
-                    containerColor = Black500,
-                    modifier = Modifier
-                        .size(
-                            width = 68.dp,
-                            height = 28.dp,
-                        )
-                        .padding(end = 8.dp),
-                ) { /* onClick */ }
-            },
-        )
+        ) {
+            DanjamTextField(value = "", onValueChange = {}, hintText = "EX) 단잠과")
+        }
     }
 }
 
-@Preview
 @Composable
-fun a() {
-    DanjamTheme {
-        UniversityInformationScreen()
+private fun InputBox(
+    title: String,
+    modifier: Modifier = Modifier,
+    textField: @Composable () -> Unit,
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineLarge,
+            color = Black950,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        textField()
     }
 }
