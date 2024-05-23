f3d8b613f693cd474e4207115e40bcefc6ea1825
Bandal <no1msh1217@gmail.com>
2024-05-23 16:42:03 +0900

    refactor: 로그인을 위한 service로 이름 변경

--- core/data/src/main/java/com/saeongjima/data/api/SignInService.kt
+++ core/data/src/main/java/com/saeongjima/data/api/SignInService.kt
@@ -1,11 +1,10 @@
 package com.saeongjima.data.api
 
 import com.saeongjima.data.api.model.SignInRequest
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-interface TokenService {
+interface SignInService {
     @POST("/login")
     suspend fun postSignIn(
         @Body
