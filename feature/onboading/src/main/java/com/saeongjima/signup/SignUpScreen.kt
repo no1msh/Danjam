32eb281fffa0527a2746d4c525d8a13e3f294cda
Bandal <no1msh1217@gmail.com>
2024-06-03 03:16:32 +0900

    refactor: SignUpScreen에서 각종 화면들 Route로 참조하게끔 변경

--- feature/onboading/src/main/java/com/saeongjima/signup/SignUpScreen.kt
+++ feature/onboading/src/main/java/com/saeongjima/signup/SignUpScreen.kt
@@ -3,8 +3,9 @@ package com.saeongjima.signup
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,62 +15,70 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.saeongjima.designsystem.component.button.MainButton
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.saeongjima.designsystem.R
+import com.saeongjima.designsystem.extension.modifier.pagerFadeTransition
 import com.saeongjima.designsystem.theme.Black200
 import com.saeongjima.designsystem.theme.DanjamTheme
 import com.saeongjima.designsystem.theme.PointColor1
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun SignUpScreen(
+fun SignUpRoute(
     modifier: Modifier = Modifier,
     onCloseClick: () -> Unit,
 ) {
-    val pagerState = rememberPagerState(pageCount = { 6 })
+    SignUpScreen(
+        modifier = modifier,
+        onCloseClick = onCloseClick,
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+internal fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    onCloseClick: () -> Unit,
+    viewModel: SignUpViewModel = hiltViewModel(),
+) {
+    val pagerState = rememberPagerState(pageCount = { SignUpProgress.entries.size })
     val coroutineScope = rememberCoroutineScope()
-    var isButtonEnabled by rememberSaveable {
-        mutableStateOf(true)
-    }
 
     Column {
         OnboardingTopAppBar(
             leadingIcon = if (pagerState.currentPage == 0) {
                 null
             } else {
-                com.saeongjima.designsystem.R.drawable.ic_back_24
+                R.drawable.ic_back_24
             },
-            trailingIcon = com.saeongjima.designsystem.R.drawable.ic_exit_24,
+            trailingIcon = R.drawable.ic_exit_24,
             leadingIconDescription = null,
             trailingIconDescription = null,
             onLeadingIconClick = {
                 coroutineScope.launch {
-                    pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                    pagerState.scrollToPage(pagerState.currentPage - 1)
                 }
             },
             onTrailingIconClick = onCloseClick,
             title = if (pagerState.currentPage == 0) "약관 동의" else null,
         )
-        Column(
-            modifier = modifier.background(MaterialTheme.colorScheme.background),
-        ) {
+        Column(modifier = modifier.background(MaterialTheme.colorScheme.background)) {
             if (pagerState.currentPage != 0) {
                 DanJamProgressBar(
-                    currentPageIndex = (pagerState.currentPage - 1).toFloat(),
-                    totalPageCount = 5,
+                    currentPageIndex = (pagerState.currentPage).toFloat(),
+                    totalPageCount = SignUpProgress.entries.size,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(start = 24.dp, end = 24.dp, top = 12.dp),
                 )
+            } else {
+                Spacer(modifier = Modifier.height(8.dp))
             }
             HorizontalPager(
                 state = pagerState,
@@ -77,71 +86,61 @@ fun SignUpScreen(
                 userScrollEnabled = false,
                 verticalAlignment = Alignment.Top,
             ) { page ->
-                when (page) {
-                    0 -> {
-                        AgreeScreen(
-                            modifier = Modifier.padding(horizontal = 24.dp),
-                            enableButton = { it: Boolean -> isButtonEnabled = it })
-                    }
-
-                    1 -> PersonalInformationScreen(
-                        modifier = Modifier.padding(
-                            top = 40.dp,
-                            start = 24.dp,
-                            end = 24.dp,
-                        ),
-                    )
-
-                    2 -> UniversityInformationScreen(
-                        modifier = Modifier.padding(
-                            top = 40.dp,
-                            start = 24.dp,
-                            end = 24.dp,
-                        ),
-                    )
-
-                    3 -> SignInInformationScreen(
-                        modifier = Modifier.padding(
-                            top = 40.dp,
-                            start = 24.dp,
-                            end = 24.dp,
-                        ),
-                    )
-
-                    4 -> UniversityCertificationScreen(
-                        modifier = Modifier.padding(
-                            top = 40.dp,
-                            start = 24.dp,
-                            end = 24.dp,
-                        ),
-                    )
-
-                    5 -> SignUpDoneScreen(
-                        modifier = Modifier.padding(
-                            top = 40.dp,
-                            start = 24.dp,
-                            end = 24.dp,
-                        ),
-                    )
-                }
-            }
-
-            MainButton(
-                text = if (pagerState.currentPage == 5) "단잠메이트 만나러 가기!" else "다음",
-                modifier = Modifier.padding(
+                val modifierWithDefaultPadding = Modifier.padding(
+                    top = 40.dp,
                     start = 24.dp,
                     end = 24.dp,
-                    top = 12.dp,
-                    bottom = 28.dp,
-                ),
-                enabled = isButtonEnabled
-            ) {
-                coroutineScope.launch {
-                    if (pagerState.currentPage == 5) {
-                    } else {
+                )
+                val onNextButtonClick: () -> Unit = {
+                    coroutineScope.launch {
                         pagerState.animateScrollToPage(pagerState.currentPage + 1)
                     }
                 }
+
+                Box(modifier = Modifier.pagerFadeTransition(page, pagerState)) {
+                    when (page) {
+                        0 -> AgreeRoute(
+                            onNextButtonClick = onNextButtonClick,
+                            modifier = Modifier.padding(horizontal = 24.dp),
+                        )
+
+                        1 -> PersonalInformationRoute(
+                            onNextButtonClick = { personalInformationUiState ->
+                                viewModel.updatePersonalInformation(personalInformationUiState)
+                                onNextButtonClick()
+                            },
+                            modifier = modifierWithDefaultPadding,
+                        )
+
+                        2 -> PersonalInformationCertificationGuideRoute(
+                            onNextButtonClick = onNextButtonClick,
+                            modifier = modifierWithDefaultPadding,
+                        )
+
+                        3 -> PersonalInformationCertificationRoute(
+                            onNextButtonClick = onNextButtonClick,
+                            modifier = modifierWithDefaultPadding,
+                        )
+
+                        4 -> UniversityInformationRoute(
+                            onNextButtonClick = onNextButtonClick,
+                            modifier = modifierWithDefaultPadding,
+                        )
+
+                        5 -> SignInInformationRoute(
+                            onNextButtonClick = onNextButtonClick,
+                            modifier = modifierWithDefaultPadding,
+                        )
+
+                        6 -> UniversityCertificationRoute(
+                            onNextButtonClick = onNextButtonClick,
+                            modifier = modifierWithDefaultPadding,
+                        )
+
+                        7 -> SignUpDoneScreen(modifier = modifierWithDefaultPadding)
+                    }
+                }
+
             }
         }
     }
@@ -156,8 +155,7 @@ fun DanJamProgressBar(
     val progress by animateFloatAsState(
         targetValue = (currentPageIndex + 1) / totalPageCount.toFloat(),
         label = "pageProgress",
-
-        )
+    )
 
     LinearProgressIndicator(
         progress = { progress },
@@ -168,10 +166,23 @@ fun DanJamProgressBar(
     )
 }
 
+enum class SignUpProgress {
+    Agree,
+    PersonalInformation,
+    PersonalCertificationGuide,
+    PersonalCertification,
+    UniversityInformation,
+    SignInInformation,
+    UniversityCertification,
+    SignUpDone,
+}
+
 @Preview
 @Composable
 private fun SignUpPreview() {
     DanjamTheme {
-        SignUpScreen(modifier = Modifier.fillMaxSize(), onCloseClick = {})
+        SignUpScreen(
+            onCloseClick = {},
+        )
     }
 }
