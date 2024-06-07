9b668fdf685dc49adc5c5de4ecc314fbcb0f0541
Bandal <no1msh1217@gmail.com>
2024-06-07 19:19:54 +0900

    feat: 유저 닉네임의 책임을 가지는 객체 구현

---
+++ core/model/src/main/java/com/saeongjima/model/account/Nickname.kt
@@ -0,0 +1,14 @@
+package com.saeongjima.model.account
+
+@JvmInline
+value class Nickname(override val value: String): NotSupportedLength {
+
+    override fun isKeepRange(): Boolean {
+        return value.length in MIN_LENGTH..MAX_LENGTH
+    }
+
+    companion object {
+        private const val MIN_LENGTH = 2
+        private const val MAX_LENGTH = 10
+    }
+}
