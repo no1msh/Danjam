4cd5f4e2f6916d20b104251a0656f82fa472a567
Bandal <no1msh1217@gmail.com>
2024-06-13 23:05:07 +0900

    feat: LoadingDialog 컴포저블 함수 구현

---
+++ core/designsystem/src/main/java/com/saeongjima/designsystem/component/dialog/LoadingDialog.kt
@@ -0,0 +1,49 @@
+package com.saeongjima.designsystem.component.dialog
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
+import com.saeongjima.designsystem.R
+import com.saeongjima.designsystem.theme.Black200
+import com.saeongjima.designsystem.theme.PointColor1
+
+@Composable
+fun LoadingDialog(
+    onDismissRequest: () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        (LocalView.current.parent as DialogWindowProvider).window.setDimAmount(0.8f)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = dimensionResource(id = R.dimen.layout_default_padding),
+                    vertical = 12.dp
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(56.dp),
+                color = PointColor1,
+                strokeWidth = 6.dp,
+                trackColor = Black200,
+                strokeCap = StrokeCap.Round,
+            )
+        }
+    }
+}
